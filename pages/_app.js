a4c7f3e576c8d6e4d34675d7a6482abc71e753b9
joonacode <joonacode@gmail.com>
2021-12-15 20:39:46 +0700

    test google tag

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { DefaultSeo } from 'next-seo';
 import '../styles/global.css';
 import TagManager from 'react-gtm-module';
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -25,6 +26,23 @@ function MyApp({ Component, pageProps }) {
           cardType: 'summary_large_image',
         }}
       />
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTA}`}
+      />
+      <Script
+        strategy='afterInteractive'
+        id='config_gtag'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTA}');
+          `,
+        }}
+      />
+
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
